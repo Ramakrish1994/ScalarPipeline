b5b38b1304d0fa6e7b459615e63d20c54c77f270
mohanaprasad1994 <mohanaprasad1994@yahoo.co.in>
2015-10-11 13:11:28 +0530

    send filename as params to main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "simulator.h"
 
-int main(){
+int main(int argc, char* argv[]){
 
-    Simulator* simulator = new Simulator("input.txt", false,true);
+
+    Simulator* simulator = new Simulator(argv[1], false,false);
 	simulator->simulate();
 	delete simulator;
 	return 1;
